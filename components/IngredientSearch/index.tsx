fc837151c95886024e422dd8ca9067226188047c
MoatShrimp <mathias.viklund@gmail.com>
2022-03-30 16:46:09 +0200

    add logic for getting current selected ingredient

--- components/IngredientSearch/index.tsx
+++ components/IngredientSearch/index.tsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
+import SelectedIngredients from '../SelectedIngredients';
 import { IngredientResponse } from './type';
 
 export default function IngredientSearch() {
   const [ingredients, setIngredients] = useState<IngredientResponse[]>();
+  const [newIngredient, setNewIngredient] = useState<string>("");
+  const [chosenIngredient, setChosenIngredient] = useState<string>("");
 
   useEffect(() => {
     fetch("https://localhost:7255/api/ingredients", { mode: 'cors' })
@@ -10,14 +13,25 @@ export default function IngredientSearch() {
       .then((data: IngredientResponse[]) => setIngredients(data));
   }, []);
 
+  const submitForm = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setChosenIngredient(newIngredient);
+
+    setNewIngredient("");
+  };
+
   return (
     <>
-      <label htmlFor="ingredient-choice">Choose an ingredient:</label>
-      <input list="ingredient-list" id="ingredient-choice" name="ingredient-choice" />
+      <form onSubmit={submitForm}>
+        <label htmlFor="ingredient-choice">Choose an ingredient:</label>
+        <input list="ingredient-list" id="ingredient-choice" name="ingredient-choice" onChange={(e) => {setNewIngredient(e.target.value)}} value={newIngredient} />
 
-      <datalist id="ingredient-list">
-        {ingredients?.map(i => <option value={i.name} key={i.id}/>)}
-      </datalist> 
+        <datalist id="ingredient-list">
+          {ingredients?.map(i => <option value={i.name} key={i.id}/>)}
+        </datalist> 
+      </form>
+
+      <SelectedIngredients newIngredient={chosenIngredient} />
     </>
   )
 }
