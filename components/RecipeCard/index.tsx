af92fef95229e25576a6289ccdb4096ea7a3e82e
MoatShrimp <mathias.viklund@gmail.com>
2022-03-31 13:17:42 +0200

    add image data to component

--- components/RecipeCard/index.tsx
+++ components/RecipeCard/index.tsx
@@ -4,9 +4,9 @@ import { RecipeCardProps } from './type';
 
 export default function RecipeCard( {title, image}:RecipeCardProps ) {
   return (
-    <>
+    <li>
       <h3>{title}</h3>
-      <Image src={image} alt={title} />
-    </>
+      <Image width={200} height={100} src={image} alt={title} />
+    </li>
   )
 }
