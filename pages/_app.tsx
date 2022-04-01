face52ef8f1ed24a66a4970810ee016923afdca0
MoatShrimp <mathias.viklund@gmail.com>
2022-04-01 09:26:57 +0200

    remove helloWorld-API call

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { useEffect, useState } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-  const [helloWorld, setHelloWorld] = useState<String>("");
-
-  useEffect( () => {
-    fetch("https://localhost:7255/api/HelloWorld", {mode: 'cors'})
-    .then(response => response.json())
-    .then((data:String) => setHelloWorld(data));
-  }, []);
-
-  return <><h1>{helloWorld}</h1><Component {...pageProps} /></>
+  return <Component {...pageProps} />
 }
 
 export default MyApp
