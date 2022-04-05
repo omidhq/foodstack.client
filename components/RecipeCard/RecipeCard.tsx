bc65dc368b6c1cc8abae4d28d649ffa78ee00e92
Leon Jardevall <leonaccoun@gmail.com>
2022-04-05 09:59:22 +0200

    style recipeCard component

--- components/RecipeCard/RecipeCard.tsx
+++ components/RecipeCard/RecipeCard.tsx
@@ -4,9 +4,9 @@ import { RecipeCardProps } from './RecipeCard.types';
 
 export default function RecipeCard({ title, image }: RecipeCardProps) {
   return (
-    <li>
-      <h3>{title}</h3>
-      <Image width={200} height={100} src={image} alt={title} />
+    <li className='border-2 rounded m-2 flex h-36'>
+      <Image className='object-none' width={120} height={120} src={image} alt={title} />
+      <h3 className='m-2'>{title}</h3>
     </li>
   )
 }
