e3db32207d2c8fc10a3cd2a5dcfa6564c05a3a09
MoatShrimp <mathias.viklund@gmail.com>
2022-04-07 11:08:53 +0200

    get ingredients with url params

--- components/RecipeResults/RecipeResult.tsx
+++ components/RecipeResults/RecipeResult.tsx
@@ -2,14 +2,29 @@ import React, { useEffect, useState } from 'react'
 import { RecipeResultsProps, RecipeResponse } from './RecipeResult.types';
 import RecipeCard from "../RecipeCard";
 import Pagination from '../Pagination';
+import { useRouter } from 'next/router';
 
 
-export default function RecipeResults({ ingredients }: RecipeResultsProps) {
+export default function RecipeResults() {
 
   const [results, setResults] = useState<RecipeResponse[]>([]);
   const [currentPage, setCurrentPage] = useState<number>(1);
 
   const maxPerPage:number = 20;
+
+  const router = useRouter()
+  const [ingredients, setIngredients] = useState<string[]>([])
+
+  useEffect(() => {
+    if (router.isReady) {
+      const query =  router.query.ingredients as any;
+      const page = router.query.page as any;
+      if (query){
+        setIngredients(query.split(','));
+        setCurrentPage(page)
+      }      
+    }
+}, [router])
 
   useEffect(() => {
     if (ingredients.length === 0) {
